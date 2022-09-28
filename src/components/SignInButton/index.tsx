9440c17e217e2dae7d2ef31cc9a6259cf6c034c6
pedrohbs00 <peuhbs@gmail.com>
2022-09-27 22:54:45 -0300

    Add: Faunadb config

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,12 +1,10 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { signIn, signOut ,useSession } from 'next-auth/react'
+import { signIn, signOut , useSession } from 'next-auth/react'
 import styles from './styles.module.scss'
 
 export function SignInButton () {
     const {data: session} = useSession()
-
-    console.log(session)
 
     return session ? (
         (
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import GitHubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
     providers: [
@@ -13,4 +17,41 @@ export default NextAuth({
               },
         }),
     ],
+
+    callbacks: {
+      async signIn({ user, account, profile, credentials }) {
+        const { email } = user
+
+        try {
+          await fauna.query(
+            q.If(
+              q.Not(
+                q.Exists(
+                  q.Match(
+                    q.Index('user_by_email'),
+                    email
+                  )
+                )
+              ),
+              q.Create(
+                q.Collection('users'),
+                {data: {email}}
+              ),
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            )
+          )
+
+          return true;
+
+        } catch (err) {
+          console.log(err)
+          return false;
+        }
+      },
+    }
 })
