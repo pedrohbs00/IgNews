21e97fb78bef1891a17d82dea6d1b520723489e2
Pedro Batista <peuhbs@gmail.com>
2023-03-27 18:51:27 -0300

    Feat: :sparkles: added links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,24 @@
-import { SignInButton } from '../SignInButton'
-import styles from './styles.module.scss'
+import Link from "next/link";
+
+import { SignInButton } from "../SignInButton";
+import styles from "./styles.module.scss";
 
 export function Header() {
-    return (
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="Ig.News" />
-                <nav>
-                    <a className={styles.active}>Home</a>
-                    <a>Posts</a>
-                </nav>
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="Ig.News" />
+        <nav>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
+        </nav>
 
-                <SignInButton />
-            </div>
-        </header>
-    )
+        <SignInButton />
+      </div>
+    </header>
+  );
 }
